7f4f54690a4ae77b876972fe74fb06b2f42f31cc
janelle-nomad <janelle.saige@gmail.com>
2016-08-05 21:00:12 -0400

    Added both metric, and imperial formulas, and modified appearance of BMI Calculator form

--- BMI_calc/BMICalculator.Designer.cs
+++ BMI_calc/BMICalculator.Designer.cs
@@ -61,10 +61,10 @@
             this.BMI_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.83607F));
             this.BMI_layout.Controls.Add(this.ImperialRadioBtn, 1, 0);
             this.BMI_layout.Controls.Add(this.metricRadioBtn, 0, 0);
-            this.BMI_layout.Controls.Add(this.HeightLbl, 0, 1);
             this.BMI_layout.Controls.Add(this.WeightLbl, 1, 1);
             this.BMI_layout.Controls.Add(this.HeightTB, 0, 2);
             this.BMI_layout.Controls.Add(this.WeightTB, 1, 2);
+            this.BMI_layout.Controls.Add(this.HeightLbl, 0, 1);
             this.BMI_layout.Location = new System.Drawing.Point(4, 4);
             this.BMI_layout.Name = "BMI_layout";
             this.BMI_layout.RowCount = 3;
@@ -77,44 +77,47 @@
             // ImperialRadioBtn
             // 
             this.ImperialRadioBtn.AutoSize = true;
-            this.ImperialRadioBtn.Checked = true;
             this.ImperialRadioBtn.Font = new System.Drawing.Font("Lora", 20F);
             this.ImperialRadioBtn.Location = new System.Drawing.Point(151, 3);
             this.ImperialRadioBtn.Name = "ImperialRadioBtn";
             this.ImperialRadioBtn.Size = new System.Drawing.Size(141, 45);
             this.ImperialRadioBtn.TabIndex = 1;
-            this.ImperialRadioBtn.TabStop = true;
             this.ImperialRadioBtn.Text = "Imperial";
             this.ImperialRadioBtn.UseVisualStyleBackColor = true;
+            this.ImperialRadioBtn.CheckedChanged += new System.EventHandler(this.ImperialRadioBtn_CheckedChanged);
             // 
             // metricRadioBtn
             // 
             this.metricRadioBtn.AutoSize = true;
+            this.metricRadioBtn.Checked = true;
             this.metricRadioBtn.Font = new System.Drawing.Font("Lora", 20F);
             this.metricRadioBtn.Location = new System.Drawing.Point(3, 3);
             this.metricRadioBtn.Name = "metricRadioBtn";
             this.metricRadioBtn.Size = new System.Drawing.Size(120, 45);
             this.metricRadioBtn.TabIndex = 0;
+            this.metricRadioBtn.TabStop = true;
             this.metricRadioBtn.Text = "Metric";
             this.metricRadioBtn.UseVisualStyleBackColor = true;
+            this.metricRadioBtn.CheckedChanged += new System.EventHandler(this.metricRadioBtn_CheckedChanged);
             // 
             // HeightLbl
             // 
             this.HeightLbl.AutoSize = true;
-            this.HeightLbl.Font = new System.Drawing.Font("Lora", 19F);
+            this.HeightLbl.Font = new System.Drawing.Font("Lora", 13F);
             this.HeightLbl.Location = new System.Drawing.Point(3, 61);
             this.HeightLbl.Name = "HeightLbl";
-            this.HeightLbl.Size = new System.Drawing.Size(98, 78);
+            this.HeightLbl.Size = new System.Drawing.Size(100, 27);
             this.HeightLbl.TabIndex = 1;
             this.HeightLbl.Text = "My Height";
+            this.HeightLbl.Click += new System.EventHandler(this.metricRadioBtn_CheckedChanged);
             // 
             // WeightLbl
             // 
             this.WeightLbl.AutoSize = true;
-            this.WeightLbl.Font = new System.Drawing.Font("Lora", 19F);
+            this.WeightLbl.Font = new System.Drawing.Font("Lora", 13F);
             this.WeightLbl.Location = new System.Drawing.Point(151, 61);
             this.WeightLbl.Name = "WeightLbl";
-            this.WeightLbl.Size = new System.Drawing.Size(104, 78);
+            this.WeightLbl.Size = new System.Drawing.Size(104, 27);
             this.WeightLbl.TabIndex = 2;
             this.WeightLbl.Text = "My Weight";
             // 
@@ -146,6 +149,7 @@
             this.CalculateBMIBtn.TabIndex = 4;
             this.CalculateBMIBtn.Text = "Calculate BMI";
             this.CalculateBMIBtn.UseVisualStyleBackColor = false;
+            this.CalculateBMIBtn.Click += new System.EventHandler(this.CalculateBMIBtn_Click);
             // 
             // ResultsTB
             // 
@@ -304,19 +308,21 @@
             // 
             // button13
             // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(235)))), ((int)(((byte)(59)))));
             this.button13.Font = new System.Drawing.Font("Lora", 11F);
             this.button13.Location = new System.Drawing.Point(200, 235);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(95, 32);
             this.button13.TabIndex = 21;
             this.button13.Text = "Backspace";
-            this.button13.UseVisualStyleBackColor = true;
+            this.button13.UseVisualStyleBackColor = false;
             this.button13.Click += new System.EventHandler(this.backspaceBtn_Click);
             // 
             // BMICalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(157)))), ((int)(((byte)(219)))));
             this.ClientSize = new System.Drawing.Size(304, 441);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.button12);
@@ -336,6 +342,7 @@
             this.Controls.Add(this.CalculateBMIBtn);
             this.Controls.Add(this.BMI_layout);
             this.Font = new System.Drawing.Font("Lora", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
             this.MaximizeBox = false;
--- BMI_calc/BMICalculator.cs
+++ BMI_calc/BMICalculator.cs
@@ -23,8 +23,9 @@ namespace BMI_calc
 {
     public partial class BMICalculator : Form
     {
-        bool heightInput = false;
-        bool weightInput = false;
+        public bool heightInput = false;
+        public bool weightInput = false;
+        private string _formulaType;
 
         public BMICalculator()
         {
@@ -35,6 +36,15 @@ namespace BMI_calc
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /***
+         * Allows the program to get the appropriate formula to use for calculateing BMI
+         */
+        public string ForumlaType
+        {
+            get { return this._formulaType; }
+            set { this._formulaType = value; }
         }
 private void HeightTB_Click(object sender, EventArgs e)
         {
@@ -99,6 +109,111 @@ private void HeightTB_Click(object sender, EventArgs e)
                 
                 return;
             }
+        }
+
+        private void metricRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            HeightLbl.Text = "My Height [CM]";
+            WeightLbl.Text = "My Weight [KG]";
+            this.ForumlaType = metricRadioBtn.Text;
+        }
+
+        private void ImperialRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            HeightLbl.Text = "My Height [IN]";
+            WeightLbl.Text = "My Weight [LBS]";
+            this.ForumlaType = ImperialRadioBtn.Text;
+        }
+
+        public void formula()
+        {
+            this._formulaType = "Imperial";
+        }
+
+        
+        public double Calculation()
+        {
+            double userBMI = 0;
+            double userWeight = Double.Parse(WeightTB.Text);
+            double userHeight = Double.Parse(HeightTB.Text);
+
+            if (this._formulaType == "Imperial")
+            {
+                            
+                userBMI = ((userWeight * 703) / (userHeight * userHeight / 100));
+
+            }
+
+            else if (this._formulaType == "Metric")
+            {
+                userBMI = (userWeight/(userHeight/100)*(userHeight/100));
+            }
+            return userBMI;
+        }
+
+        public string bmiOutput()
+        {
+            double userBmi = Calculation();
+            string bmiOutput = "";
+
+            if (userBmi < 18.5)
+            {
+                bmiOutput = "Underweight";
+            }
+
+            else if (userBmi >=18.5 && userBmi <= 24.9)
+            {
+                bmiOutput = "Normal";
+            }
+
+            else if (userBmi >= 25 && userBmi <= 29.9)
+            {
+                bmiOutput = "Overweight";
+            }
+
+            else if (userBmi >= 30)
+            {
+                bmiOutput = "Obese";
+            }
+            return bmiOutput;
+        }
+
+        public string UserResult()
+        {
+            double userBMI = Calculation();
+
+            string userResult = "";
+
+            if (userBMI < 18.5)
+            {
+                userResult = "Less than 18.5";
+            }
+
+            else if (userBMI >= 18.5 && userBMI <= 24.9)
+            {
+                userResult = "Between 18.5 and 24.9";
+            }
+
+            else if (userBMI >= 25 && userBMI <= 29.9)
+            {
+                userResult = "Between 25 and 29.9";
+            }
+
+            else if (userBMI >= 30)
+            {
+                userResult = "30 or greater";
+            }
+
+            return userResult;
+        }
+
+        private void CalculateBMIBtn_Click(object sender, EventArgs e)
+        {
+            ResultsTB.Text = "";
+            OutputTB.Text = "";
+
+            OutputTB.Text = UserResult();
+            ResultsTB.Text = bmiOutput();
         }
     }
 }
